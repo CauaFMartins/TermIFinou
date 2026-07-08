0ac7a796e9b44ec08ae7672fada4b7f18c0d8b5d
Cauã Martins <cauafmartins01@gmail.com>
2026-07-08 10:08:08 -0300

    aaaa

---
+++ project/Thumb.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+func _ready():
+	set_process_input(true)
+
+	OS.window_fullscreen = true
+
+func _input(event):
+
+	if event.is_action_pressed("ui_accept"):
+		get_tree().change_scene("res://project/ControlsWarning.tscn")
+
+	elif event is InputEventMouseButton:
+		if event.pressed:
+			get_tree().change_scene("res://project/ControlsWarning.tscn")
